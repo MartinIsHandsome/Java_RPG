fcf30ec3b134e325687948a8dd1360c93fd9bc39
marti <marti@DESKTOP-LJTIM30>
2024-11-04 23:13:41 +0100

    Today (4th Nov) I made it to the animation part.

--- src/entity/playerClass.java
+++ src/entity/playerClass.java
@@ -3,6 +3,10 @@ package entity;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import firstPackage.gamePanel;
 import firstPackage.keyControl;
@@ -15,35 +19,77 @@ public class playerClass extends Entity {
 		this.kH = kH;
 		this.panel = panel;
 		setDefaultValues();
+		getPlayerImage();
 	}
 
 	public void setDefaultValues() {
 		x = 100;
 		y = 100;
 		speed = 4;
+		direction = "down";
+	}
+
+	public void getPlayerImage() {
+
+		try {
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/Front.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/Front2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/Back.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/Back2.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/Left.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/Left2.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/Right.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/Right2.png"));
+
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void update() {
 		if (kH.downPress == true) {
+			direction = "up";
 			y += speed;
 		}
 
 		else if (kH.upPress == true) {
+			direction = "down";
 			y -= speed;
-			System.out.print("Working");
 		} else if (kH.leftPress == true) {
+			direction = "left";
 			x -= speed;
 		}
 
 		else if (kH.rightPress == true) {
+			direction = "right";
 			x += speed;
 		}
 
 	}
 
 	public void draw(Graphics2D g2) {
+//		g2.setColor(Color.RED);
+//		g2.fillRect(x, y, panel.tileSize, panel.tileSize);
 
-		g2.setColor(Color.RED);
-		g2.fillRect(x, y, panel.tileSize, panel.tileSize);
+		BufferedImage image = null;
+		switch (direction) {
+		case "up":
+			image = up1;
+			break;
+		case "down":
+			image = down1;
+			break;
+		case "left":
+			image = left1;
+			break;
+		case "right":
+			image = right1;
+			System.out.print("Hi");
+			break;
+		}
+
+		g2.drawImage(image, x, y, panel.tileSize, panel.tileSize, panel);
 	}
 }
